ae4ed62b78ec9969ecd6cefe3e5aaef0c03529a4
Santiago Monsalve <smonsal@bancolombia.com.co>
2025-06-25 00:31:42 -0500

    Inicio de proyecto final

--- proyectofinal/src/app/app.routes.ts
+++ proyectofinal/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'courses', component: CoursesComponent},
+    {path: 'admin', component: AdminComponent}
+];
